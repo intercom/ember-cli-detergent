8231959ab7a8fb90595de3c2842cfeae046a525f
paddyobrien <paddy@intercom.io>
2017-03-26 19:07:42 -0700

    add index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 /* jshint node: true */
+/* eslint-env node */
 'use strict';
 
+let HtmlSafeChecker = require('./lib/html-safe-checker');
+
 module.exports = {
-  name: 'ember-cli-high-tide'
+  name: 'ember-cli-high-tide',
+
+  postBuild(result) {
+    HtmlSafeChecker.verify();
+  }
 };
